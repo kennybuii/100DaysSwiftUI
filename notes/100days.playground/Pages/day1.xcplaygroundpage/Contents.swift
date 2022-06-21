f0d64a6c2772c8117694489a70ed9b41c9727efc
kennybuii <kennytbui11@gmail.com>
2022-06-21 12:31:43 -0700

    organizing

--- notes/100days.playground/Pages/day1.xcplaygroundpage/Contents.swift
+++ notes/100days.playground/Pages/day1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,21 @@
-//: [Previous](@previous)
+import Cocoa
+//variables are created with var =
+var varTest = 1_000
+//constants are created with let =
+let constTest = 1_001
 
-import Foundation
+// multi-line strings require """
+//stuff u want to multi-line
+//""" where the """ are all on solo lines
+var string = """
+i'm testing out "strings"
+"""
 
-var greeting = "Hello, playground"
+//functions like .count, .uppercased(), .hasPrefix("requirement"), and .hasSuffix("requirement")
+print(string.count)
 
-//: [Next](@next)
+//type safety for ints and doubles, need to cast
+var x = 1
+var y = 1.1
+var z = x + Int(y)
+print(z)
---
+++ notes/100days.playground/Pages/day2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,2 @@
+import Cocoa
+//assign boolean by using let, ex: let good = true
